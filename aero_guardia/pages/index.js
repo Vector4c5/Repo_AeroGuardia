23234c83fb1cf4b749a635e54c20394d02725291
Vector4c5 <hernandez435627@gmail.com>
2026-03-27 00:29:07 -0600

    .

--- aero_guardia/pages/index.js
+++ aero_guardia/pages/index.js
@@ -1,10 +1,53 @@
+import StarAnimation from "@/Components/common/StartAnimation";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div 
-    clasname = "Relative flex flex-col w-full h-auto bg-white">
-      Hola Mundo
+    <div
+      className="relative z-0 flex min-h-screen w-full flex-col items-center justify-center text-black"
+    >
+      <StarAnimation />
+
+      <div className="fixed inset-0 z-0 w-full h-full pointer-events-none">
+        <img
+          src="/Fondo_Hangares.jpg"
+          alt="Inicio"
+          className="w-full h-full object-cover opacity-60"
+        />
+      </div>
+
+      <div className="w-6/12 h-64 flex items-center justify-start bg-white rounded-lg 
+      shadow-lg shadow-black z-10 overflow-hidden">
+
+        <img
+          src="/Logo_AeroGuardia.png"
+          alt=""
+          className="h-full w-auto object-contain pointer-events-none"
+        />
+
+        <div 
+        className="w-full h-full flex flex-col items-center justify-center mx-4">
+          <h2 className="text-xl font-bold text-gray-800 text-center">
+            Tu seguridad es nuestra prioridad
+          </h2>
+          <p className="text-gray-600 text-center mt-2">
+            Confía en nuestra experiencia y tecnología para proteger tus operaciones aéreas.
+          </p>
+          <Link
+            href="/landing.js"
+            className="group relative mt-4 overflow-hidden rounded-lg border-2 border-black bg-yellow-300 px-6 py-2 transition duration-500 ease-in-out hover:scale-105"
+          >
+            <span className="relative z-10 text-black transition duration-500 ease-in-out">
+              Iniciar Sesion
+            </span>
+            <span className="absolute top-0 left-0 h-full w-full bg-amber-400 transform -translate-x-full group-hover:translate-x-0 transition duration-500 ease-in-out"></span>
+          </Link>
+        </div>
+
+
+      </div>
+
+
     </div>
   );
 }
---
+++ aero_guardia/pages/landing.js
@@ -0,0 +1,12 @@
+import StarAnimation from "@/Components/common/StartAnimation";
+import Link from "next/link";
+
+export default function landing() {
+  return (
+    <div
+      className="relative z-0 flex min-h-screen w-full flex-col items-center justify-center text-black"
+    >
+
+    </div>
+   ); 
+}
